2881286742f92ae994a53d95ce6ee63219aa644d
Yuji OKAMOTO <okamyuji@gmail.com>
2026-03-21 12:20:58 +0900

    chore: configure Android/iOS platform settings
    - Android: RECORD_AUDIO, FOREGROUND_SERVICE, biometric permissions, largeHeap, noCompress onnx, minSdk 23
    - iOS: NSMicrophoneUsageDescription, NSFaceIDUsageDescription, UIBackgroundModes audio, platform 13.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,10 +24,14 @@ android {
         applicationId = "com.example.koememo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+    }
+
+    aaptOptions {
+        noCompress += "onnx"
     }
 
     buildTypes {
